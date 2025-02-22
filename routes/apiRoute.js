c686819ad8b0fb6c790e29fdfc09dd3fa20e3e21
= <=>
2025-02-22 11:49:01 -0500

    Mejoro control

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -6,10 +6,18 @@ let statusToResponse = false
 const apiRouter = Router()
 
 apiRouter.get("/api-led/:status", (req,res)=>{
-    const {status} = req.params()
-    if(typeof status === "boolean"){
+    const {status} = req.params
+    if(typeof Boolean(status) === "boolean"){
         statusToResponse = status
+        console.log("Alla")
+        return res.status(200).json({
+         status: "ok"
+        })
     }
+    console.log("Entro aca")
+    return res.status(400).json({
+        status: "Error"
+    })
 })
 
 apiRouter.get("/api-led", (req, res) => {
